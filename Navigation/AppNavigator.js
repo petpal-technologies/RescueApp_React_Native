aeea2cb0f523f8f4d444449f7c8519b81d53d981
Chris Horton <Chris.horton@sbcglobal.net>
2019-05-13 21:13:54 -0700

    Login and some navigation

--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -3,12 +3,6 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from 'MainTabNavigator';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
-}));
-
 import { Navigation } from 'react-native-navigation'
 
 export const goToAuth = () => Navigation.setRoot({
